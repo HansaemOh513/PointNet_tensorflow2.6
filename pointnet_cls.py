9a0001a19fd97f5adbc8fe76c8fd289023ad355b
Hansaem Oh <147702795+HansaemOh513@users.noreply.github.com>
2024-03-27 21:19:52 +0900

    Update pointnet_cls.py

--- pointnet_cls.py
+++ pointnet_cls.py
@@ -6,16 +6,36 @@ import tensorflow as tf
 import h5py
 from tensorflow.keras.optimizers import Adam
 from tqdm import tqdm
+import argparse
+from utils import loss_graph, shuffle_data, rotate_point_cloud, data_loader
+
+TRAIN_FILES = [
+"data/modelnet40_ply_hdf5_2048/ply_data_train0.h5",
+"data/modelnet40_ply_hdf5_2048/ply_data_train1.h5",
+"data/modelnet40_ply_hdf5_2048/ply_data_train2.h5",
+"data/modelnet40_ply_hdf5_2048/ply_data_train3.h5",
+# "data/modelnet40_ply_hdf5_2048/ply_data_train4.h5"
+]
+
+TEST_FILES = [
+"data/modelnet40_ply_hdf5_2048/ply_data_test0.h5",
+"data/modelnet40_ply_hdf5_2048/ply_data_test1.h5"
+]
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
+parser.add_argument('--model', default='pointnet_cls', help='Model name: pointnet_cls or pointnet_cls_basic [default: pointnet_cls]')
+parser.add_argument('--max_iteration', type=int, default=100, help='Iteration to run [default: 100]')
+parser.add_argument('--batch_size', type=int, default=32, help='Batch Size during training [default: 32]')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='Initial learning rate [default: 0.0001]')
+parser.add_argument('--dropout_rate', type=float, default=0.7, help='Dropout_rate [default: 0.7]')
+args = parser.parse_args()
+batch_size = args.batch_size
 # 1번 cuda device 사용 : Use first cuda device
-os.environ["CUDA_VISIBLE_DEVICES"] = "1" 
-file_path = './data/modelnet40_ply_hdf5_2048/ply_data_train0.h5'
-with h5py.File(file_path, 'r') as file:
-    label = file['label']
-    data_dataset = file['data']
-    data_labels  = label[:]
-    data_values = data_dataset[:]
-# tensorflow 모델 투입을 위해서 차원확장 : Expandng demension to put in a tensorflow model 
-point_cloud = np.expand_dims(data_values, -1)
+os.environ["CUDA_VISIBLE_DEVICES"] = f"{args.gpu}"
+
+
+
 # pointcloud 모델 신경망 : The pointcloud neural network architecture
 class network(tf.keras.Model):
     def __init__(self):
@@ -48,7 +68,7 @@ class network(tf.keras.Model):
         self.model_2_dense1 = tf.keras.layers.Dense(512, activation='relu')
         self.model_2_dense2 = tf.keras.layers.Dense(256, activation='relu')
         self.model_2_dense3 = tf.keras.layers.Dense(40, activation=None)
-
+        self.dropout = tf.keras.layers.Dropout(args.dropout_rate)
     def call(self, inputs):
         y = self.input_conv1(inputs)
         y = self.input_conv2(y)
@@ -87,63 +107,74 @@ class network(tf.keras.Model):
 model = network()
 model.build(input_shape=(None, 2048, 3, 1))
 model.summary()
-# 실험에 의해 확인한 learning_rate : The learning rate is chosen by experiments
-optimizer = Adam(learning_rate=0.00003)
+# 실험에 의해 확인한 learning_rate 0.0001 : The learning rate 0.0001 is chosen by experiments
+optimizer = Adam(learning_rate=args.learning_rate)
 
 def train(max_iteration):
     losses = []
     accuracies = []
     model_params = model.trainable_variables
-    num_batches = len(point_cloud) // 32
+    
     for iteration in range(max_iteration):
         total_loss = []
         total_accuracy = []
-        with tqdm(total=num_batches, desc=f'iteration {iteration + 1}', unit='batch') as pbar:
-            for i in range(0, len(point_cloud), 32):
-                # batch size를 32로 조정 : The batch size of data is set as 32
-                batch_size=32
-                point_cloud_batch = point_cloud[i:i+batch_size]
-                with tf.GradientTape() as tape:
-                    tape.watch(model_params)
-                    y = model(point_cloud_batch)
-                    
-                    labels = tf.one_hot([data_labels[i:i+batch_size]], 40)
-                    loss = tf.reduce_mean(tf.square(tf.squeeze(labels, axis=[0, 2]) - y))
-                    y_pred = tf.argmax(y, axis=1)
-                    labels_pred = tf.argmax(tf.squeeze(labels, axis=[0, 2]), axis=1)
-                    accuracy = tf.reduce_mean(tf.cast(tf.equal(labels_pred, y_pred), dtype=tf.float32))
-                grads = tape.gradient(loss, model_params)
-                optimizer.apply_gradients(zip(grads, model_params))
-                pbar.update(1)
-                pbar.set_postfix({'Loss': loss.numpy()})
-
-                total_loss.append(loss)
-                total_accuracy.append(accuracy)
         
-        iteration_loss = np.mean(np.array(total_loss))
-        iteration_accuracy = np.mean(np.array(total_accuracy))
-        print(f'Iteration {iteration + 1}, Loss: {iteration_loss:.5f}, Accuracy: {iteration_accuracy:.5f}')
-        losses.append(iteration_loss)
-        accuracies.append(iteration_accuracy)
+        num_batches = (2048 * 4) // batch_size
+        with tqdm(total=num_batches, desc=f'iteration {iteration + 1}', unit='batch') as pbar:
+            for data in TRAIN_FILES:
+                data_point_cloud, data_labels = data_loader(data)
+                for i in range(0, len(data_point_cloud), batch_size):
+                    point_cloud_batch = data_point_cloud[i:i+batch_size]
+                    labels_batch = data_labels[i:i+batch_size]
+                    # 데이터 순서 셔플
+                    shuffled_point_cloud_batch, shuffled_labels_batch = shuffle_data(point_cloud_batch, labels_batch)
+                    # 데이터 회전
+                    rotated_point_cloud_batch = rotate_point_cloud(shuffled_point_cloud_batch)
+                    rotated_point_cloud_batch = np.expand_dims(rotated_point_cloud_batch, -1)
+                    with tf.GradientTape() as tape:
+                        tape.watch(model_params)
+                        y = model(rotated_point_cloud_batch)
+                        labels = tf.one_hot(shuffled_labels_batch, 40)
+                        loss = tf.reduce_mean(tf.square(tf.squeeze(labels) - y))
+                        y_pred = tf.argmax(y, axis=1)
+                        labels_pred = tf.argmax(tf.squeeze(labels), axis=1)
+                        accuracy = tf.reduce_mean(tf.cast(tf.equal(labels_pred, y_pred), dtype=tf.float32))
+                    grads = tape.gradient(loss, model_params)
+                    optimizer.apply_gradients(zip(grads, model_params))
+                    pbar.update(1)
+                    pbar.set_postfix({'Loss': loss.numpy()})
+                    total_loss.append(loss)
+                    total_accuracy.append(accuracy)
+                validation_loss = []
+                validation_accuracy = []
+            for data in TEST_FILES:
+                data_point_cloud, data_labels = data_loader(data)
+                for i in range(0, len(data_point_cloud), batch_size):
+                    point_cloud_batch = data_point_cloud[i:i+batch_size]
+                    labels_batch = data_labels[i:i+batch_size]
+                    # 데이터 순서 셔플
+                    shuffled_point_cloud_batch, shuffled_labels_batch = shuffle_data(point_cloud_batch, labels_batch)
+                    # 데이터 회전
+                    rotated_point_cloud_batch = rotate_point_cloud(shuffled_point_cloud_batch)
+                    rotated_point_cloud_batch = np.expand_dims(rotated_point_cloud_batch, -1)
+                    y = model(rotated_point_cloud_batch)
+                    labels = tf.one_hot(shuffled_labels_batch, 40)
+                    loss = tf.reduce_mean(tf.square(tf.squeeze(labels) - y))
+                    y_pred = tf.argmax(y, axis=1)
+                    labels_pred = tf.argmax(tf.squeeze(labels), axis=1)
+                    accuracy = tf.reduce_mean(tf.cast(tf.equal(labels_pred, y_pred), dtype=tf.float32))
+                    validation_loss.append(loss)
+                    validation_accuracy.append(accuracy)
+            iteration_validation_loss = np.mean(np.array(validation_loss))
+            iteration_validation_accuracy = np.mean(np.array(validation_accuracy))
+            iteration_loss = np.mean(np.array(total_loss))
+            iteration_accuracy = np.mean(np.array(total_accuracy))
+            print(f'Iteration {iteration + 1}, Loss: {iteration_loss:.5f}, Validation Loss: {iteration_validation_loss:.5f}, Accuracy: {iteration_accuracy:.5f}, Validation Accuracy: {iteration_validation_accuracy:.5f}')
+            losses.append(iteration_loss)
+            accuracies.append(iteration_accuracy)
     return losses, accuracies
-max_iteration = 1000
+max_iteration = args.max_iteration
 losses, accuracies = train(max_iteration)
 # 훈련 곡선 : training curve
-plt.figure(figsize=(10, 5))
 
-plt.subplot(1, 2, 1)
-plt.plot(losses, label='Loss')
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
-plt.title('Training Loss')
-plt.legend()
-
-plt.subplot(1, 2, 2)
-plt.plot(accuracies, color='orange', label='Accuracy')
-plt.xlabel('Iteration')
-plt.ylabel('Accuracy')
-plt.title('Training Accuracy')
-plt.legend()
-
-plt.tight_layout()
-plt.show()
+loss_graph(losses, accuracies)
